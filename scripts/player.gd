7ab393a04c0de39801cb23f353c1360cedbd7b48
The Douck <thedouckdev@gmail.com>
2025-07-08 16:56:27 +0200

    added extra gameplay features

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,4 +1,5 @@
 extends CharacterBody2D
+@onready var basic_tile_map: TileMapLayer = $"../TileMaps/BasicTileMap"
 
 const SPEED = 110.0
 const JUMP_VELOCITY = 200.0
@@ -7,11 +8,23 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	
+	# Handle dying falling of the map.
+	if position.y > basic_tile_map.get_used_rect().end.y + 100:
+		print("you died")  # Or handle respawning, etc.
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = -JUMP_VELOCITY
-
+	
+	# Handle doublejump/airjump.
+	if Input.is_action_just_pressed("jump") and not is_on_floor():
+		if not has_meta("doublejump_used"):
+			velocity.y = -JUMP_VELOCITY
+			set_meta("doublejump_used", true)
+	# Reset doublejump when touching the floor.
+	if is_on_floor() and has_meta("doublejump_used"):
+		remove_meta("doublejump_used")
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("move_left", "move_right")
