f1f9f37a1634e96f4a2f55f00913663c5f637f58
Viktor Popp <ViktorPopp.dev@gmail.com>
2025-07-10 11:13:37 +0200

    Added auto-tiling tilemaps

--- game/game.gd
+++ game/game.gd
@@ -53,13 +53,13 @@ func _on_join_pressed():
 func add_player(pid):
 	var player = PLAYER.instantiate()
 	player.name = str(pid)
-	player.global_position = $Level.get_child(players.size()).global_position
+	player.global_position = $Spawnpoints.get_child(players.size()).global_position
 	players.append(player)
 	
 	return player
 
 func get_random_spawnpoint():
-	return $Level.get_children().pick_random().global_position
+	return $Spawnpoints.get_children().pick_random().global_position
 
 func get_local_ip() -> String:
 	for addr in IP.get_local_addresses():
